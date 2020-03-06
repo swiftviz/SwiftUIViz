a87ddadfa5250e4feafb9d90d653003acffce473
Joe Heck <heckj@mac.com>
2020-03-06 11:35:56 -0800

    codecov enabling, cleanup

--- Package.swift
+++ Package.swift
@@ -8,26 +8,28 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v11),
-        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftUIViz",
-            targets: ["SwiftUIViz"])
+            targets: ["SwiftUIViz"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swiftviz/SwiftViz", from: "0.0.0")
+        .package(url: "https://github.com/swiftviz/SwiftViz", from: "0.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUIViz",
-            dependencies: ["SwiftViz"]),
+            dependencies: ["SwiftViz"]
+        ),
         .testTarget(
             name: "SwiftUIVizTests",
-            dependencies: ["SwiftUIViz", "SwiftViz"])
+            dependencies: ["SwiftUIViz", "SwiftViz"]
+        ),
     ]
 )
