d59e1067e4dc2cbc131219493fc1354672cf0052
Joe Heck <heckj@mac.com>
2020-03-06 14:53:45 -0800

    basic initialiation tests

---
+++ Tests/SwiftUIVizTests/HorizontalAxisViewTests.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//
+//
+//  HorizontalAxisViewTests by Joseph Heck on 3/6/20.
+//
+
+import SwiftUI
+@testable import SwiftUIViz
+import SwiftViz
+import ViewInspector
+import XCTest
+
+extension HorizontalAxisView: Inspectable {}
+// reference to using ViewInspector to poke around:
+// https://github.com/nalexn/ViewInspector/blob/master/guide.md
+
+final class HorizontalAxisViewTests: XCTestCase {
+    func testHorizontalAxisView_init() throws {
+        let exampleView = HorizontalAxisView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false),
+                                             leftInset: 25.0,
+                                             rightInset: 25.0)
+        XCTAssertNotNil(exampleView)
+    }
+}
