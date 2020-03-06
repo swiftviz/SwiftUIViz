da47704b437be66055b352847bb333f83071d1b5
Joe Heck <heckj@mac.com>
2020-03-06 11:55:50 -0800

    trying out ViewInspector for testing SwiftUI views

---
+++ Tests/SwiftUIVizTests/ViewInspectorTrialTest.swift
@@ -0,0 +1,29 @@
+//
+//  ViewInspectorTrialTest.swift
+//
+//  Created by Joseph Heck on 3/6/20.
+//
+
+import SwiftUI
+import XCTest
+import ViewInspector
+@testable import SwiftUIViz
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+extension ContentView: Inspectable {}
+// reference to using ViewInspector to poke around:
+// https://github.com/nalexn/ViewInspector/blob/master/guide.md
+
+final class ContentViewTests: XCTestCase {
+
+    func testStringValue() throws {
+        let sut = ContentView()
+        let value = try sut.inspect().text().string()
+        XCTAssertEqual(value, "Hello, world!")
+    }
+}
